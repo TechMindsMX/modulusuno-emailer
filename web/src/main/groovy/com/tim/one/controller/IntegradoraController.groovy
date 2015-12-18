ed8b5a296b6c4ff7165fda698465baa0150fbe2f
says33 <says.rodriguez@gmail.com>
2015-12-18 12:35:41 -0600

    #3 se crea el controller para el envio de una orden de deposito

--- web/src/main/groovy/com/tim/one/controller/IntegradoraController.groovy
+++ web/src/main/groovy/com/tim/one/controller/IntegradoraController.groovy
@@ -23,6 +23,8 @@ import com.tim.one.validator.CommandValidator
 import com.tim.one.service.MessageService
 import com.tim.one.enums.MessageType
 import com.tim.one.constant.ApplicationConstants
+import com.tim.one.command.DepositOrderCommand
+import com.tim.one.bean.DepositOrderBean
 
 import org.apache.commons.logging.Log
 import org.apache.commons.logging.LogFactory
@@ -32,23 +34,23 @@ import org.apache.commons.logging.LogFactory
 class IntegradoraController {
 
   @Autowired
-	CommandValidator validator
+  CommandValidator validator
   @Autowired
-	MessageService messageDispatcher
+  MessageService messageDispatcher
   @Autowired
-	Properties properties
+  Properties properties
 
-	Log log = LogFactory.getLog(getClass())
+  Log log = LogFactory.getLog(getClass())
 
   @RequestMapping(method = POST, value = "/newUser")
-	@ResponseBody
-	public ResponseEntity<String> newUser(@RequestBody String json){
-		NameCommand command = new Gson().fromJson(json, NameCommand.class)
-		log.info "Sending email: ${command.dump()}"
+  @ResponseBody
+  public ResponseEntity<String> newUser(@RequestBody String json){
+    NameCommand command = new Gson().fromJson(json, NameCommand.class)
+    log.info "Sending email: ${command.dump()}"
 
-		if(!validator.isValid(command)){
-	    return new ResponseEntity<String>("Error: " + ErrorCode.VALIDATOR_ERROR.ordinal(), HttpStatus.BAD_REQUEST);
-		}
+    if(!validator.isValid(command)){
+      return new ResponseEntity<String>("Error: " + ErrorCode.VALIDATOR_ERROR.ordinal(), HttpStatus.BAD_REQUEST);
+    }
 
     NewUserBean bean = new NewUserBean()
     bean.setEmail(properties.getProperty(ApplicationConstants.INTEGRADORA_ADMIN));
@@ -56,17 +58,17 @@ class IntegradoraController {
     bean.setType(MessageType."${command.type}")
     messageDispatcher.message(bean)
     return new ResponseEntity<String>("OK", HttpStatus.OK)
-	}
+  }
 
   @RequestMapping(method = POST, value = "/register")
-	@ResponseBody
-	public ResponseEntity<String> register(@RequestBody String json){
-		RegisterCommand command = new Gson().fromJson(json, RegisterCommand.class)
-		log.info "Sending email: ${command.dump()}"
+  @ResponseBody
+  public ResponseEntity<String> register(@RequestBody String json){
+    RegisterCommand command = new Gson().fromJson(json, RegisterCommand.class)
+    log.info "Sending email: ${command.dump()}"
 
-		if(!validator.isValid(command)){
-	    return new ResponseEntity<String>("Error: " + ErrorCode.VALIDATOR_ERROR.ordinal(), HttpStatus.BAD_REQUEST)
-		}
+    if(!validator.isValid(command)){
+      return new ResponseEntity<String>("Error: " + ErrorCode.VALIDATOR_ERROR.ordinal(), HttpStatus.BAD_REQUEST)
+    }
 
     ForgotPasswordBean bean = new ForgotPasswordBean()
     bean.setToken(command.getToken())
@@ -74,17 +76,17 @@ class IntegradoraController {
     bean.setType(MessageType.REGISTER)
     messageDispatcher.message(bean)
     return new ResponseEntity<String>("OK", HttpStatus.OK)
-	}
+  }
 
   @RequestMapping(method = POST, value = "/forgot")
-	@ResponseBody
-	public ResponseEntity<String> forgot(@RequestBody String json){
-		ForgotPasswordCommand command = new Gson().fromJson(json, ForgotPasswordCommand.class)
-		log.info "Sending email: ${command.dump()}"
+  @ResponseBody
+  public ResponseEntity<String> forgot(@RequestBody String json){
+    ForgotPasswordCommand command = new Gson().fromJson(json, ForgotPasswordCommand.class)
+    log.info "Sending email: ${command.dump()}"
 
-		if(!validator.isValid(command)){
-	    return new ResponseEntity<String>("Error: " + ErrorCode.VALIDATOR_ERROR.ordinal(), HttpStatus.BAD_REQUEST)
-		}
+    if(!validator.isValid(command)){
+      return new ResponseEntity<String>("Error: " + ErrorCode.VALIDATOR_ERROR.ordinal(), HttpStatus.BAD_REQUEST)
+    }
 
     ForgotPasswordBean bean = new ForgotPasswordBean()
     bean.setToken(command.getToken())
@@ -92,17 +94,41 @@ class IntegradoraController {
     bean.setType(MessageType.FORGOT_PASSWORD)
     messageDispatcher.message(bean)
     return new ResponseEntity<String>("OK", HttpStatus.OK)
-	}
+  }
+
+  @RequestMapping(method = POST, value = "/depositOrder")
+  @ResponseBody
+  public ResponseEntity<String> depositOrderByCompany(@RequestBody String json){
+    DepositOrderCommand command = new Gson().fromJson(json, DepositOrderCommand.class)
+    log.info "Sending email: ${command.dump()}"
+
+    if(!validator.isValid(command)){
+      return new ResponseEntity<String>("Error: " + ErrorCode.VALIDATOR_ERROR.ordinal(), HttpStatus.BAD_REQUEST)
+    }
+
+    DepositOrderBean bean = new DepositOrderBean()
+    bean.setEmail(command.email)
+    bean.setName(command.name)
+    bean.setAccount(command.account)
+    bean.setMessage(command.message)
+    bean.setBank(command.bank)
+    bean.setAccountBank(command.accountBank)
+    bean.setUrl(command.url)
+    bean.setType(MessageType.DEPOSIT_ORDER)
+    messageDispatcher.message(bean)
+    return new ResponseEntity<String>("OK", HttpStatus.OK)
+
+  }
 
   @RequestMapping(method = POST, value = "/companyIntegrated")
-	@ResponseBody
-	public ResponseEntity<String> companyAssignedBuyer(@RequestBody String json){
-		CompanyIntegratedCommand command = new Gson().fromJson(json, CompanyIntegratedCommand.class)
-		log.info "Sending email: ${command.dump()}"
+  @ResponseBody
+  public ResponseEntity<String> companyAssignedBuyer(@RequestBody String json){
+    CompanyIntegratedCommand command = new Gson().fromJson(json, CompanyIntegratedCommand.class)
+    log.info "Sending email: ${command.dump()}"
 
-		if(!validator.isValid(command)){
-	    return new ResponseEntity<String>("Error: " + ErrorCode.VALIDATOR_ERROR.ordinal(), HttpStatus.BAD_REQUEST)
-		}
+    if(!validator.isValid(command)){
+      return new ResponseEntity<String>("Error: " + ErrorCode.VALIDATOR_ERROR.ordinal(), HttpStatus.BAD_REQUEST)
+    }
 
     CompanyIntegratedBean bean = new CompanyIntegratedBean()
     bean.setEmail(command.emailResponse)
@@ -112,17 +138,17 @@ class IntegradoraController {
     bean.setType(MessageType.COMPANY_INTEGRATED)
     messageDispatcher.message(bean)
     return new ResponseEntity<String>("OK", HttpStatus.OK)
-	}
+  }
 
   @RequestMapping(method = POST, value = "/clientProvider")
-	@ResponseBody
-	public ResponseEntity<String> clientProvider(@RequestBody String json){
-		NameCommand command = new Gson().fromJson(json, NameCommand.class)
-		log.info "Sending email: ${command.dump()}"
+  @ResponseBody
+  public ResponseEntity<String> clientProvider(@RequestBody String json){
+    NameCommand command = new Gson().fromJson(json, NameCommand.class)
+    log.info "Sending email: ${command.dump()}"
 
-		if(!validator.isValid(command)){
-	    return new ResponseEntity<String>("Error: " + ErrorCode.VALIDATOR_ERROR.ordinal(), HttpStatus.BAD_REQUEST);
-		}
+    if(!validator.isValid(command)){
+      return new ResponseEntity<String>("Error: " + ErrorCode.VALIDATOR_ERROR.ordinal(), HttpStatus.BAD_REQUEST);
+    }
 
     NewUserBean bean = new NewUserBean()
     bean.setEmail(command.getEmail())
@@ -131,6 +157,6 @@ class IntegradoraController {
     bean.setType(MessageType."${command.type}")
     messageDispatcher.message(bean)
     return new ResponseEntity<String>("OK", HttpStatus.OK)
-	}
+  }
 
 }
