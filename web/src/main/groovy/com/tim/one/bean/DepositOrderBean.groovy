5044360343e9bb91f44bd14756f9daf6489f87a6
says33 <says.rodriguez@gmail.com>
2015-12-18 12:38:08 -0600

    #3 se agrega el bean para el objeto de una orden de deposito

---
+++ web/src/main/groovy/com/tim/one/bean/DepositOrderBean.groovy
@@ -0,0 +1,16 @@
+package com.tim.one.bean
+
+import com.tim.one.enums.MessageType
+
+class DepositOrderBean implements EmailBean {
+
+  String email
+  String name
+  String account
+  String message
+  String bank
+  String accountBank
+  String url
+  MessageType type
+
+}
