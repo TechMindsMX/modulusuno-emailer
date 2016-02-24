e90f2d17d4208793138432d9485f41fd9adb4665
says33 <says.rodriguez@gmail.com>
2016-02-24 11:54:34 -0600

    #9 se agrega el metodo para regsitrar nuevas ciuentas para le proyecto modulus-uno

--- web/src/main/groovy/com/tim/one/controller/ModulusController.groovy
+++ web/src/main/groovy/com/tim/one/controller/ModulusController.groovy
@@ -16,11 +16,24 @@ import org.springframework.web.bind.annotation.ResponseBody
 
 import com.google.gson.Gson
 import com.tim.one.bean.ErrorCode
-import com.tim.one.enums.MessageType
 import com.tim.one.bean.AccountBean
 import com.tim.one.command.AccountCommand
-import com.tim.one.service.MessageService
+import com.tim.one.command.NewUserCommand
+import com.tim.one.command.NameCommand
+import com.tim.one.bean.NewUserBean
+import com.tim.one.command.RegisterCommand
+import com.tim.one.bean.ForgotPasswordBean
+import com.tim.one.command.ForgotPasswordCommand
+import com.tim.one.bean.CompanyIntegratedBean
+import com.tim.one.command.CompanyIntegratedCommand
 import com.tim.one.validator.CommandValidator
+import com.tim.one.service.MessageService
+import com.tim.one.enums.MessageType
+import com.tim.one.constant.ApplicationConstants
+import com.tim.one.command.DepositOrderCommand
+import com.tim.one.bean.DepositOrderBean
+import com.tim.one.command.SaleOrderCommand
+import com.tim.one.bean.SaleOrderBean
 
 /**
  * @author josdem
@@ -58,5 +71,23 @@ class ModulusController {
     messageDispatcher.message(bean)
     return new ResponseEntity<String>("OK", HttpStatus.OK)
 	}
+
+	@RequestMapping(method = POST, value = "/register")
+  @ResponseBody
+  public ResponseEntity<String> register(@RequestBody String json){
+    RegisterCommand command = new Gson().fromJson(json, RegisterCommand.class)
+    log.info "Sending email: ${command.dump()}"
+
+    if(!validator.isValid(command)){
+      return new ResponseEntity<String>("Error: " + ErrorCode.VALIDATOR_ERROR.ordinal(), HttpStatus.BAD_REQUEST)
+    }
+
+    ForgotPasswordBean bean = new ForgotPasswordBean()
+    bean.setToken(command.getToken())
+    bean.setEmail(command.getEmail())
+    bean.setType(MessageType.REGISTER_MODULUS)
+    messageDispatcher.message(bean)
+    return new ResponseEntity<String>("OK", HttpStatus.OK)
+  }
 
 }
