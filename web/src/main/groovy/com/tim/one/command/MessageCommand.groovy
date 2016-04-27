b4c4fc588164e27c39326ee8575ba2484ef39f6d
temocpalma <temopalmag@gmail.com>
2016-04-27 12:14:31 -0500

    #12 Agregando documentación de message controller

--- web/src/main/groovy/com/tim/one/command/MessageCommand.groovy
+++ web/src/main/groovy/com/tim/one/command/MessageCommand.groovy
@@ -4,14 +4,19 @@ import javax.validation.constraints.NotNull
 
 import org.hibernate.validator.constraints.Email
 import org.hibernate.validator.constraints.SafeHtml
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
 
+@ApiModel(value="MessageCommand", description="Message command model")
 class MessageCommand implements Command {
-	
+
 	@Email
 	@NotNull
+  @ApiModelProperty(value = "Email")
 	String email
-	
+
 	@SafeHtml
+  @ApiModelProperty(value="Message")
 	String message
-	
+
 }
