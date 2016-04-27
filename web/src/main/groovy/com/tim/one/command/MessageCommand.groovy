8d695dd9ab3d85af82456a1c2fb519d2cd563bd9
temocpalma <temopalmag@gmail.com>
2016-04-27 15:56:31 -0500

    #13 Elimino documentación del command

--- web/src/main/groovy/com/tim/one/command/MessageCommand.groovy
+++ web/src/main/groovy/com/tim/one/command/MessageCommand.groovy
@@ -4,19 +4,14 @@ import javax.validation.constraints.NotNull
 
 import org.hibernate.validator.constraints.Email
 import org.hibernate.validator.constraints.SafeHtml
-import io.swagger.annotations.ApiModel
-import io.swagger.annotations.ApiModelProperty
 
-@ApiModel(value="MessageCommand", description="Message command model")
 class MessageCommand implements Command {
 
 	@Email
 	@NotNull
-  @ApiModelProperty(value = "Email")
 	String email
 
 	@SafeHtml
-  @ApiModelProperty(value="Message")
 	String message
 
 }
