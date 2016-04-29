c1d09d65fb472dd489d77271e6822f55cf3b5134
josdem <joseluis.delacruz@gmail.com>
2016-04-29 13:06:33 -0500

    #11 Corrigiendo ortografía

--- web/src/main/groovy/com/tim/one/constant/ApplicationConstants.groovy
+++ web/src/main/groovy/com/tim/one/constant/ApplicationConstants.groovy
@@ -9,5 +9,5 @@ interface ApplicationConstants {
   static final String ADMIN_EMAIL = "admin.email"
   static final String INTEGRADORA_ADMIN = "integradora.admin"
   static final String MODULUSUNO_TARGET = 'modulusuno.target'
-  static final String MODULUSUNO_HOME = 'mosulusuno.home'
+  static final String MODULUSUNO_HOME = 'modulusuno.home'
 }
